cc35adaa72bb13952fbc21186d6b6938b137ef4f
gabriel schiavo <gabriel@lewagon.org>
2019-05-09 17:03:27 -0300

    init script for marketing funil

--- app/mailers/funil_mailer.rb
+++ app/mailers/funil_mailer.rb
@@ -1,22 +1,28 @@
-@name = []
-@name
+# Declare a Hash with students info
+@PROSPECS = 
 
-def first_contact()
+
+@FIRST_NAME = []
+@CITY = 
+@CAMP_START = [] # Date Ruby Class
+@CAMP_END = [] # Ruby Date Class
+@BOOKING_URL = ""
+
+def first_contact(prospecs)
 	return 
-	"Olá #{$FIRST_NAME}
+	"Olá #{FIRST_NAME}
 	 Obrigado por sua candidatura e pelo interesse no nosso bootcamp.
-	 Nossa próxima data em $CITY será de $CAMP_START a $CAMP_END.
+	 Nossa próxima data em #{CITY} será de #{CAMP_START} a #{CAMP_END}.
+	 Para continuar seu processo de candidatura é necessário agendar uma entrevista para discutirmos expecativas e termos certeza de que poderemos atendê-las. 
 
-Para continuar seu processo de candidatura é necessário agendar uma entrevista para discutirmos expecativas e termos certeza de que poderemos atendê-las. 
+	 Por favor , **[agende um horário]($BOOKING_URL)** para sua entrevista.
 
-Por favor , **[agende um horário]($BOOKING_URL)** para sua entrevista.
+   Para sua informação, um dos requisitos para se matricular no Le Wagon é ter completado o
+   [curso de Ruby]($CODECADEMY_RUBY_URL) no CodeCademy (é gratuito).
+   Agende sua entrevista e já pode começar a trabalhar no CodeCademy!
 
-Para sua informação, um dos requisitos para se matricular no Le Wagon é ter completado o
-[curso de Ruby]($CODECADEMY_RUBY_URL) no CodeCademy (é gratuito).
-Agende sua entrevista e já pode começar a trabalhar no CodeCademy!
+   Fique à vontade para responder a este e-mail caso tenha alguma dúvida.
 
-Fique à vontade para responder a este e-mail caso tenha alguma dúvida.
-
-Um abraço!
-"
+   Um abraço!
+   "
 end
